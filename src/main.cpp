2248405722d02668a4627807975f4dbc3b3a70fe
Emilio J. Padron <emilioj@udc.es>
2013-12-02 19:14:09 +0100

    IGM OpenGL stuff: Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,198 @@
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include <math.h>
+
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+using namespace std;
+
+
+char* loadFile(string fname, GLint &fSize)
+{
+    ifstream::pos_type size;
+    char * memblock;
+    string text;
+
+    // file read based on example in cplusplus.com tutorial
+    ifstream file (fname, ios::in|ios::binary|ios::ate);
+    if (file.is_open())
+    {
+        size = file.tellg();
+        fSize = (GLuint) size;
+        memblock = new char [size];
+        file.seekg (0, ios::beg);
+        file.read (memblock, size);
+        file.close();
+        cout << "file " << fname << " loaded" << endl;
+        text.assign(memblock);
+    }
+    else
+    {
+        cout << "Unable to open file " << fname << endl;
+        exit(1);
+    }
+    return memblock;
+}
+
+
+// printShaderInfoLog
+// From OpenGL Shading Language 3rd Edition, p215-216
+// Display (hopefully) useful error messages if shader fails to compile
+void printShaderInfoLog(GLint shader)
+{
+    int infoLogLen = 0;
+    int charsWritten = 0;
+    GLchar *infoLog;
+
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLen);
+
+    // should additionally check for OpenGL errors here
+
+    if (infoLogLen > 0)
+    {
+        infoLog = new GLchar[infoLogLen];
+        // error check for fail to allocate memory omitted
+        glGetShaderInfoLog(shader,infoLogLen, &charsWritten, infoLog);
+        cout << "InfoLog:" << endl << infoLog << endl;
+        delete [] infoLog;
+    }
+
+    // should additionally check for OpenGL errors here
+}
+
+void initShaders()
+{
+    GLuint p, f, v;
+
+    char *vs,*fs;
+
+    v = glCreateShader(GL_VERTEX_SHADER);
+    f = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // load shaders & get length of each
+    GLint vlen;
+    GLint flen;
+    vs = loadFile("../src/vertexShader.glsl",vlen);
+    fs = loadFile("../src/fragmentShader.glsl",flen);
+
+    const char * vv = vs;
+    const char * ff = fs;
+
+    glShaderSource(v, 1, &vv,&vlen);
+    glShaderSource(f, 1, &ff,&flen);
+
+    GLint compiled;
+
+    glCompileShader(v);
+    glGetShaderiv(v, GL_COMPILE_STATUS, &compiled);
+    if (!compiled)
+    {
+        cout << "Vertex shader not compiled." << endl;
+        printShaderInfoLog(v);
+    }
+
+    glCompileShader(f);
+    glGetShaderiv(f, GL_COMPILE_STATUS, &compiled);
+    if (!compiled)
+    {
+        cout << "Fragment shader not compiled." << endl;
+        printShaderInfoLog(f);
+    }
+
+    p = glCreateProgram();
+
+    glBindAttribLocation(p,0, "in_Position");
+    glBindAttribLocation(p,1, "in_Color");
+    glBindAttribLocation(p,2, "in_Normals");
+
+    glAttachShader(p,v);
+    glAttachShader(p,f);
+
+    glLinkProgram(p);
+    glUseProgram(p);
+
+    delete [] vs; // dont forget to free allocated memory
+    delete [] fs; // we allocated this in the loadFile function...
+
+}
+
+void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+void display()
+{
+    static const GLfloat red[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+    glClearBufferfv(GL_COLOR, 0, red);
+}
+
+int main(int argc, char **argv)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* create context */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "testogl", NULL, NULL);
+    if (!window)
+    {
+        cout << "glfw not able to create a window, aborting." << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    //Glew Init
+    glewExperimental = GL_TRUE;
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        cout << "glewInit failed, aborting." << endl;
+        exit (1);
+    }
+
+    cout << "OpenGL version: " << glGetString(GL_VERSION) << endl;
+    cout << "GLSL version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+    cout << "GLEW version: " << glewGetString(GLEW_VERSION) << endl;
+
+    //initShaders();
+
+    /*glfw Callbacks*/
+    glfwSetKeyCallback(window, keyboardCallback);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        display();
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+
+}
